57ae8657b3dc4ef7e20d0dbc3791a8d4e4261fd7
Felippe <69222834+FelippeGB@users.noreply.github.com>
2025-11-16 22:55:59 -0300

    Remove pip install commands from ETFWebScrapping.py
    Removed installation commands for dependencies.

--- ETFWebScrapping.py
+++ ETFWebScrapping.py
@@ -1,7 +1,3 @@
-!pip install webdriver-manager
-!pip install selenium
-!pip install html5lib
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
